45450a4306827286b8d3117981c131e3b107a233
Backroom8816 <140431054+Backroom8816@users.noreply.github.com>
2024-06-13 11:13:01 +0200

    solve repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let stringToRepeat = "";
+    if (times < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < times; i++) {
+            stringToRepeat += word;
+        }
+        return stringToRepeat;
+    }
 };
 
 // Do not edit below this line
